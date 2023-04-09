c2156260a9f0cde3e104256e494e48e33b65347a
Sirojiddin <jorisdandii@gmail.com>
2023-04-10 05:46:59 +0900

    first commit

---
+++ masala_1.js
@@ -0,0 +1,18 @@
+
+// berilgan numberlarni 0 dan boshlab qiymatini chiqaradigan function.
+
+// let abs = [10, 3, -4, 20]
+
+// const positiveSum = arr => abs.filter(value => value > 0).reduce((a, b) => a + b, 0);
+
+// console.log(positiveSum());
+
+// ******************************************************************************************************
+
+// musbat sonlarimizni doyim manfiy qilib chiqarib beradi.
+
+// const makeNegative = (num) => {
+//   return -Math.abs(num);
+// };
+
+// console.log(makeNegative());
